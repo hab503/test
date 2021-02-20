ed3d065ea5b0a79aff0cb97debe9c7f36ed783a6
hab503 <38175028+hab503@users.noreply.github.com>
2021-02-20 17:19:02 +0800

    Update phicomm-k3.diy-part2.sh

--- phicomm-k3.diy-part2.sh
+++ phicomm-k3.diy-part2.sh
@@ -2,4 +2,4 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改linux内核
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/bcm53xx/Makefile
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/bcm53xx/Makefile
