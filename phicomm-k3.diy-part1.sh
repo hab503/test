b51e8b0da4293a7edff0738a2df00aba8c0780e4
hab503 <38175028+hab503@users.noreply.github.com>
2021-03-12 01:13:39 +0800

    Update phicomm-k3.diy-part1.sh

--- phicomm-k3.diy-part1.sh
+++ phicomm-k3.diy-part1.sh
@@ -1,2 +1,3 @@
 sed -i '$a src-git leanp https://github.com/coolsnowwolf/lede' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git openappfilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
