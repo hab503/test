8bcfc301914fae0f1249a9b04a9b333131a4286f
hab503 <38175028+hab503@users.noreply.github.com>
2021-07-19 16:50:04 +0800

    Create phicomm-k3-lean.diy-part1.sh

---
+++ phicomm-k3-lean.diy-part1.sh
@@ -0,0 +1,2 @@
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git openappfilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
